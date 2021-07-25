81f8b61e03968047db95b69d168ffa7d6a69cbf3
InalDJ <inaldjasheev@gmail.com>
2021-07-25 12:54:46 +0300

    P-005. refactor dtoValidator - Validator has been refactored.

--- src/main/java/com/java/springportfolio/util/DtoValidator.java
+++ src/main/java/com/java/springportfolio/util/DtoValidator.java
@@ -13,92 +13,75 @@ import com.java.springportfolio.exception.PortfolioException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import static com.java.springportfolio.entity.VoteCategory.COMMENTVOTE;
+import static com.java.springportfolio.entity.VoteCategory.POSTVOTE;
 import static com.java.springportfolio.entity.VoteType.DOWNVOTE;
 import static com.java.springportfolio.entity.VoteType.UPVOTE;
-
+import static org.springframework.util.StringUtils.isEmpty;
 
 @Slf4j
 @Component
 public class DtoValidator {
 
+    private static final int TOPIC_NAME_LENGTH = 100;
+    private static final int TOPIC_DESCRIPTION_LENGTH = 250;
+    private static final int USERNAME_LENGTH = 40;
+    private static final int PASSWORD_LENGTH = 40;
+    private static final int TEXT_LENGTH = 500;
+    private static final int POST_NAME_LENGTH = 100;
+    private static final int DESCRIPTION_LENGTH = 500;
+
     public void validateTopicRequest(TopicRequest topicRequest) {
         if (topicRequest == null) {
             throw new PortfolioException("Topic request cannot be null!");
         }
-        if (topicRequest.getName() == null || topicRequest.getName().equals(" ")) {
-            throw new PortfolioException("Topic name cannot be empty or null!");
-        }
+        validateMandatoryField(topicRequest.getName(), TOPIC_NAME_LENGTH, "Topic name");
+        validateMandatoryField(topicRequest.getDescription(), TOPIC_DESCRIPTION_LENGTH, "Topic name");
     }
 
     public void validateRegisterRequest(RegisterRequest registerRequest) {
         if (registerRequest == null) {
             throw new PortfolioException("Register request cannot be empty or null!");
         }
-        if (registerRequest.getUsername() == null || registerRequest.getUsername().equals(" ")) {
-            throw new PortfolioException("User name cannot be empty or null!");
-        }
-        if (registerRequest.getPassword() == null || registerRequest.getPassword().equals(" ")) {
-            throw new PortfolioException("Password cannot be empty or null!");
-        }
-        if (registerRequest.getEmail() == null || !(registerRequest.getEmail().contains("@") && registerRequest.getEmail().contains("."))) {
-            throw new PortfolioException("Email is invalid!");
-        }
+        validateMandatoryField(registerRequest.getUsername(), USERNAME_LENGTH, "User name");
+        validateMandatoryField(registerRequest.getPassword(), PASSWORD_LENGTH, "Password");
+        validateEmail(registerRequest.getEmail());
     }
 
     public void validateLoginRequest(LoginRequest loginRequest) {
         if (loginRequest == null) {
             throw new PortfolioException("Login request cannot be null!");
         }
-        if (loginRequest.getUsername() == null || loginRequest.getUsername().equals(" ")) {
-            throw new PortfolioException("User name cannot be empty or null!");
-        }
-        if (loginRequest.getPassword() == null || loginRequest.getPassword().equals(" ")) {
-            throw new PortfolioException("Password cannot be empty or null!");
-        }
+        validateMandatoryField(loginRequest.getUsername(), USERNAME_LENGTH, "User name");
+        validateMandatoryField(loginRequest.getPassword(), PASSWORD_LENGTH, "Password");
     }
 
     public void validateRefreshTokenRequest(RefreshTokenRequest refreshTokenRequest) {
         if (refreshTokenRequest == null) {
             throw new PortfolioException("Refresh token request request cannot be null!");
         }
-        if (refreshTokenRequest.getRefreshToken() == null || refreshTokenRequest.getRefreshToken().equals(" ")) {
-            throw new PortfolioException("Refresh token cannot be empty or null!");
-        }
-        if (refreshTokenRequest.getUsername() == null || refreshTokenRequest.getUsername().equals(" ")) {
-            throw new PortfolioException("User name cannot be empty or null!");
-        }
+        validateMandatoryField(refreshTokenRequest.getRefreshToken(), "Refresh token");
+        validateMandatoryField(refreshTokenRequest.getUsername(), USERNAME_LENGTH, "User name");
     }
 
     public void validateCommentRequest(CommentRequest commentRequest) {
         if (commentRequest == null) {
             throw new PortfolioException("Comment request cannot be null!");
         }
-        if (commentRequest.getText() == null || commentRequest.getText().equals(" ")) {
-            throw new PortfolioException("Comment text cannot be null or empty!");
-        } else if (commentRequest.getText().length() > 20) {
-            throw new PortfolioException("Comment text is too long!");
-        }
-        if (commentRequest.getPostId() == null) {
-            throw new PortfolioException("Post id cannot be null!");
-        }
+        validateMandatoryField(commentRequest.getText(), TEXT_LENGTH, "Text");
+        // TODO temporary solution
+        validateMandatoryField(commentRequest.getPostId().toString(), "Post id");
     }
 
     public void validatePostRequest(PostRequest postRequest) {
         if (postRequest == null) {
             throw new PortfolioException("Post request cannot be null!");
         }
-        if (postRequest.getPostName() == null || postRequest.getPostName().equals(" ")) {
-            throw new PortfolioException("Post name cannot be empty or null!");
-        } else if (postRequest.getPostName().length() > 20) {
-            throw new PortfolioException("Post name is too long!");
-        }
-        if (postRequest.getTopicId() == null) {
-            throw new PortfolioException("Topic id cannot be null!");
-        }
-        //temporary
-        if (postRequest.getDescription() != null && postRequest.getDescription().length() > 30) {
-            throw new PortfolioException("Description is too long!");
-        }
+        validateMandatoryField(postRequest.getPostName(), POST_NAME_LENGTH, "Post name");
+
+        // TODO temporary solution
+        validateMandatoryField(postRequest.getTopicId().toString(), "Topic id");
+        validateMandatoryField(postRequest.getDescription(), DESCRIPTION_LENGTH, "Description");
     }
 
     public void validateVoteRequest(VoteRequest voteRequest) {
@@ -122,8 +105,27 @@ public class DtoValidator {
         if (voteCategory == null) {
             throw new PortfolioException("Vote category cannot be null!");
         }
-        if (!voteCategory.equals(VoteCategory.COMMENTVOTE) && !voteCategory.equals(VoteCategory.POSTVOTE)) {
+        if (!voteCategory.equals(COMMENTVOTE) && !voteCategory.equals(POSTVOTE)) {
             throw new PortfolioException("Vote category is invalid!");
+        }
+    }
+
+    private void validateMandatoryField(String field, int maxLength, String fieldName) {
+        validateMandatoryField(field, maxLength, fieldName);
+        if (field.length() > maxLength) {
+            throw new PortfolioException(fieldName + " is too long (" + field.length() + "). Max length is - '" + maxLength + "'!");
+        }
+    }
+
+    private void validateMandatoryField(String field, String fieldName) {
+        if (field == null || isEmpty(field)) {
+            throw new PortfolioException("'" + fieldName + "'" + " is empty or null. '" + fieldName + "' cannot be empty or null!");
+        }
+    }
+
+    private void validateEmail(String email) {
+        if (email == null || !(email.contains("@") && email.contains("."))) {
+            throw new PortfolioException("Email is invalid!");
         }
     }
 }
